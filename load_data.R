db2b91df958f815636c3f0390c0029c350c72c5e
janikmiet <jani.k.miettinen@gmail.com>
2021-02-25 16:02:28 +0200

    edited workflow more but not fully rdy

--- load_data.R
+++ load_data.R
@@ -1,10 +1,81 @@
-# Read dataset and wrangle
-
+# Download dataset and wrangle
+library(httr)
+library(jsonlite)
+library(tibble)
 
 ## SIMPLE ------
+
+## Get data ----
+
+if(TRUE){
+  url <- "https://clinicaltrials.gov/api/query/full_studies?expr=finland&min_rnk=1&max_rnk=100&fmt=json"
+  res <- fromJSON(content(GET(url)))
+  df <- as_tibble(res$FullStudiesResponse$FullStudies)
+  trials <- dplyr::tibble(
+    # = BASIC INFOS
+    NCTId = df$Study$ProtocolSection$IdentificationModule$NCTId, ## id
+    BriefTitle = df$Study$ProtocolSection$IdentificationModule$BriefTitle, ## title
+    OfficialTitle = df$Study$ProtocolSection$IdentificationModule$OfficialTitle,
+    Acronym = df$Study$ProtocolSection$IdentificationModule$Acronym,
+    OrgFullName = df$Study$ProtocolSection$IdentificationModule$Organization$OrgFullName,
+    OrgClass = df$Study$ProtocolSection$IdentificationModule$Organization$OrgClass,
+    # = DATES
+    StartDate=df$Study$ProtocolSection$StatusModule$StartDateStruct$StartDate,
+    StartDateType=df$Study$ProtocolSection$StatusModule$StartDateStruct$StartDateType,
+    PrimaryCompletionDate=df$Study$ProtocolSection$StatusModule$PrimaryCompletionDateStruct$PrimaryCompletionDate,
+    PrimaryCompletionDateType=df$Study$ProtocolSection$StatusModule$PrimaryCompletionDateStruct$PrimaryCompletionDateType,
+    # = STATUS
+    OverallStatus=df$Study$ProtocolSection$StatusModule$OverallStatus,
+    LastKnownStatus=df$Study$ProtocolSection$StatusModule$LastKnownStatus,
+    WhyStopped=df$Study$ProtocolSection$StatusModule$WhyStopped,
+    # = RESPONSIBILITIES
+    ResponsiblePartyType=df$Study$ProtocolSection$SponsorCollaboratorsModule$ResponsibleParty$ResponsiblePartyType,
+    ResponsiblePartyInvestigatorFullName=df$Study$ProtocolSection$SponsorCollaboratorsModule$ResponsibleParty$ResponsiblePartyInvestigatorFullName,
+    ResponsiblePartyInvestigatorTitle=df$Study$ProtocolSection$SponsorCollaboratorsModule$ResponsibleParty$ResponsiblePartyInvestigatorTitle,
+    ResponsiblePartyInvestigatorAffiliation=df$Study$ProtocolSection$SponsorCollaboratorsModule$ResponsibleParty$ResponsiblePartyInvestigatorAffiliation,
+    # = SPONSOR
+    LeadSponsorName=df$Study$ProtocolSection$SponsorCollaboratorsModule$LeadSponsor$LeadSponsorName,
+    LeadSponsorClass=df$Study$ProtocolSection$SponsorCollaboratorsModule$LeadSponsor$LeadSponsorClass,
+    # = COLLABORATORS
+    Collaborators=df$Study$ProtocolSection$SponsorCollaboratorsModule$CollaboratorList$Collaborator, # df
+    # = DESCRIPTION AND SUMMARY
+    BriefSummary=df$Study$ProtocolSection$DescriptionModule$BriefSummary,
+    DetailedDescription=df$Study$ProtocolSection$DescriptionModule$DetailedDescription,
+    # =
+    Condition=df$Study$ProtocolSection$ConditionsModule$ConditionList$Condition, #df
+    Keyword=df$Study$ProtocolSection$ConditionsModule$KeywordList$Keyword,   # df
+    # =
+    StudyType=df$Study$ProtocolSection$DesignModule$StudyType,
+    Phase=df$Study$ProtocolSection$DesignModule$PhaseList$Phase,
+    # =
+    EligibilityCriteria=df$Study$ProtocolSection$EligibilityModule$EligibilityCriteria,
+    HealthyVolunteers=df$Study$ProtocolSection$EligibilityModule$HealthyVolunteers,
+    Gender=df$Study$ProtocolSection$EligibilityModule$Gender,
+    MinimumAge=df$Study$ProtocolSection$EligibilityModule$MinimumAge,
+    MaximumAge=df$Study$ProtocolSection$EligibilityModule$MaximumAge,
+    StudyPopulation=df$Study$ProtocolSection$EligibilityModule$StudyPopulation,
+    SamplingMethod=df$Study$ProtocolSection$EligibilityModule$SamplingMethod,
+    GenderBased=df$Study$ProtocolSection$EligibilityModule$GenderBased,
+    GenderDescription=df$Study$ProtocolSection$EligibilityModule$GenderDescription,
+    # =
+    # OverallOfficial=df$Study$ProtocolSection$ContactsLocationsModule$OverallOfficialList$OverallOfficial,
+    # Location=df$Study$ProtocolSection$ContactsLocationsModule$LocationList$Location,
+    # CentralContact=df$Study$ProtocolSection$ContactsLocationsModule$CentralContactList$CentralContact,
+    # =
+    Reference=df$Study$ProtocolSection$ReferencesModule$ReferenceList$Reference,
+    SeeAlsoLink=df$Study$ProtocolSection$ReferencesModule$SeeAlsoLinkList$SeeAlsoLink
+  )
+  # Create URL link
+  trials$url <- paste0("trials/", trials$NCTId, ".html")
+  trials$Link <- paste0("<a href='",trials$url,"'>",trials$url,"</a>")
+  # readr::write_csv2(trials, file = paste0("./data/",Sys.Date(), "_trials.csv"))
+  saveRDS(trials, file = paste0("./data/",Sys.Date(), "_trials.rds"))
+}
+
+
+
 
 ## Read the latest file
 fils <- list.files("data/")
 latest <- fils[order(format(as.Date(substr(fils, 1, 10), format = "%Y-%m-%d")), decreasing = T)[1]]
-d <- readr::read_csv2(paste0("data/", latest))
-# d <- readr::read_csv2("./data/simple/2020-10-05_clinical_trials.csv") ## manual file read
+d <- readRDS(paste0("data/", latest))
--- run.R
+++ run.R
@@ -1,4 +1,4 @@
-## GLOBALDATA CLINICAL TRIALS WEBPAGE/DATABASE
+## CLINICAL TRIALS WEBPAGE/DATABASE
 # This script creates clinical trials webpages from CSV file
 
 library(tidyverse)
@@ -9,6 +9,9 @@ clean_folders <- TRUE # cleans ./temp and ./output folders before rendering file
 ## Help function to recode NA values
 check_na <- function(x) {
   ifelse(is.na(x), "NA", x)
+}
+check_null <- function(x) {
+  ifelse(is.null(x[[1]]), "NA", x)
 }
 
 ## LOAD DATA AND CREATE FOLDERS ----
@@ -46,79 +49,85 @@ for (i in 1:nrow(d)) {
   # replace keywords and description
   template <- stringr::str_replace_all(string = template, 
                                        pattern = "xxx-Trial-Identifier-xxx",
-                                       replacement = check_na(d1$`NCT Number`))
+                                       replacement = check_na(d1$NCTId))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Trial-Title-xxx",
-                                       replacement = check_na(d1$Title))
+                                       replacement = check_na(d1$BriefTitle))
+  template <- stringr::str_replace_all(string = template,
+                                       pattern = "xxx-Trial-Title2-xxx",
+                                       replacement = check_na(d1$OfficialTitle))
+  template <- stringr::str_replace_all(string = template,
+                                       pattern = "xxx-Trial-Description-xxx",
+                                       replacement = check_na(d1$DetailedDescription))
+  template <- stringr::str_replace_all(string = template,
+                                       pattern = "xxx-Keywords-xxx",
+                                       replacement = unlist(check_null(d1$Keyword)))
   # template <- stringr::str_replace_all(string = template,
-  #                                      pattern = "xxx-Trial-Description-xxx",
-  #                                      replacement = check_na(d1$))
+  #                                      pattern = "xxx-Indication-xxx",
+  #                                      replacement = check_na(d1$Indication))
   template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Therapy-Area-xxx",
-                                       replacement = check_na(d1$`Therapy Area`))
+                                       pattern = "xxx-Trial-LastKnownStatus-xxx",
+                                       replacement = as.character(check_na(d1$LastKnownStatus)))
   template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Indication-xxx",
-                                       replacement = check_na(d1$Indication))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Trial-Status-xxx",
-                                       replacement = as.character(check_na(d1$`Trial Status`)))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Trial-Phase-xxx",
-                                       replacement = as.character(check_na(d1$`Trial Phase`)))
+                                       pattern = "xxx-Trial-OverallStatus-xxx",
+                                       replacement = as.character(check_na(d1$OverallStatus)))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Trial-Phase-xxx",
+  #                                      replacement = as.character(unlist(check_na(d1$Phase)))) ### check this
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Sponsor-Name-xxx",
-                                       replacement = check_na(d1$`Sponsor Name`))
+                                       replacement = check_na(d1$LeadSponsorName))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Sponsor-Type-xxx",
-                                       replacement = check_na(d1$`Sponsor Type`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Collaborator-xxx",
-                                       replacement = ifelse(is.na(d1$Collaborator), "NA", d1$Collaborator))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Collaborator-Type-xxx",
-                                       replacement = ifelse(is.na(d1$`Collaborator Type`), "NA", d1$`Collaborator Type`))
+                                       replacement = check_na(d1$LeadSponsorClass))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Collaborator-xxx",
+  #                                      replacement = unlist(check_null(d1$Collaborators))) ## DATAFRAME
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Collaborator-Type-xxx",
+  #                                      replacement = ifelse(is.na(d1$`Collaborator Type`), "NA", d1$`Collaborator Type`))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Study-Design-xxx",
-                                       replacement = ifelse(is.na(d1$`Study Design`), "NA", d1$`Study Design`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Hypothesis-xxx",
-                                       replacement = check_na(d1$`Study Hypothesis`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Purpose-xxx",
-                                       replacement = check_na(d1$`Purpose`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Primary-Objective-xxx", 
-                                       replacement = check_na(d1$`Primary Objective(s)`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Secondary-Objective-xxx",
-                                       replacement = check_na(d1$`Secondary Objective(s)`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Responsible-Authority-xxx",
-                                       replacement = check_na(d1$`Responsible Authority`))
+                                       replacement = check_na(d1$StudyType))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Hypothesis-xxx",
+  #                                      replacement = check_na(d1$Hypoth))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Purpose-xxx",
+  #                                      replacement = check_na(d1$))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Primary-Objective-xxx", 
+  #                                      replacement = check_na(d1$`Primary Objective(s)`))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Secondary-Objective-xxx",
+  #                                      replacement = check_na(d1$`Secondary Objective(s)`))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Responsible-Authority-xxx",
+  #                                      replacement = check_na(d1$`Responsible Authority`))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Age-Minimum-xxx",
-                                       replacement = check_na(d1$`Age (Minimum Eligibility)`))
+                                       replacement = check_na(d1$MinimumAge))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Age-Maximum-xxx",
-                                       replacement = check_na(d1$`Age (Maximum Eligibility)`))
+                                       replacement = check_na(d1$MaximumAge))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Gender-xxx",
-                                       replacement = check_na(d1$`Gender`))
+                                       replacement = check_na(d1$Gender))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Healthy-Subject-xxx",
-                                       replacement = check_na(d1$`Healthy Subject(s)`))
+                                       replacement = check_na(d1$HealthyVolunteers))
   template <- stringr::str_replace_all(string = template,
                                        pattern = "xxx-Participants-Criteria-Inclusion-xxx",
-                                       replacement = check_na(d1$`Participants Criteria (Inclusion)`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Participants-Criteria-Exclusion-xxx",
-                                       replacement = check_na(d1$`Participants Criteria (Exclusion)`))
-  template <- stringr::str_replace_all(string = template,
-                                       pattern = "xxx-Subject-Type-xxx",
-                                       replacement = check_na(d1$`Subject(s) Type`))
+                                       replacement = check_na(d1$EligibilityCriteria))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Participants-Criteria-Exclusion-xxx",
+  #                                      replacement = check_na(d1$`Participants Criteria (Exclusion)`))
+  # template <- stringr::str_replace_all(string = template,
+  #                                      pattern = "xxx-Subject-Type-xxx",
+  #                                      replacement = check_na(d1$`Subject(s) Type`))
   
   # save as new file
-  writeLines(template, paste0("./temp/trials/", d1$`Trial Identifier`,".Rmd"))
+  writeLines(template, paste0("./temp/trials/", d1$NCTId,".Rmd"))
 }
 
 ## RENDER RMD TO HTML  ./OUTPUT ----
@@ -133,5 +142,5 @@ rmarkdown::render("index.Rmd", output_dir = "output/")
 ## UPLOAD HTML FILES ----
 
 # move files to kapsi
-# system("scp -r ./output/* janikmiet@kapsi.fi:sites/janimiettinen.fi/www/clinicaltrials/")
+# system("scp -r ./output/* neurocenterfinland@neurocenterfinland.fi-h.seravo.com:/home/neurocenterfinland/wordpress/htdocs/kliiniset-tutkimukset")
 
